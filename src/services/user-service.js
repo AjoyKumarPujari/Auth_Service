183e700589bb0d3c4a26870bdbca4ce3d32d4cd1
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-01-04 00:19:10 +0530

    Added user isAuth function

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -40,6 +40,25 @@ class UserService{
             throw error;
         }
     }
+
+    async isAuthenticated (token){
+        try {
+            const response = this.verifyToken(token);
+            if(!response){
+                throw { error: "invalid Token"}
+            }
+            const user = this.userRepository.getById(response.id);
+            if(!user){
+                throw { error: "No user with the corrosponding token exists"};
+            }
+            return user.id;
+        } catch (error) {
+            console.log("Something went wrong in the service layer while Authenticate");
+            throw error;
+        }
+    }
+
+
      createToken(user) {
         try {
             const result = jwt.sign(user, JWT_KEY,{expiresIn: '1h'});
