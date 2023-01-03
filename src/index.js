6dc0a0663c1e9d6c0740a9985fe4f0f4c028ac1d
AjoyKumarPujari <ajoykumarpujari@gmail.com>
2023-01-03 20:33:09 +0530

    Addes JWT create and verify logic

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ const app = express();
 
 const { PORT } = require('./config/serverConfig');
 
-const apiRoutes = require('./routes/index')
+const apiRoutes = require('./routes/index');
+
+const UserService = require ('./services/user-service');
 
 //const UserRepository = require('./repository/user-repository');
 
@@ -19,9 +21,19 @@ const prepareAndStartServer = () => {
 
  app.listen(PORT, async() => {
     console.log(`Server Get Started on Port: ${PORT}` );
-   //  const repo = new UserRepository();
-   //  const response = await repo.getById(2);
-   //  console.log(response);
+
+    const service = new UserService();
+    // const newToken = service.createToken({email:'John@admin.com', id:1});
+    // console.log("New Token is ", newToken);
+
+    // const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IkpvaG5AYWRtaW4uY29tIiwiaWQiOjEsImlhdCI6MTY3Mjc1Nzg0OSwiZXhwIjoxNjcyNzYxNDQ5fQ.-4OQrBdiuEM28sJATDzmePY41Y_-AZo2t1YuO_EgeUQ";
+    // const response = service.verifyToken(token);
+    // console.log(response);
+
+   
+    // const repo = new UserRepository();
+    // const response = await repo.getById(2);
+    // console.log(response);
  });
 }
 
